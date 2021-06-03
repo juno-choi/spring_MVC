3b8234fec4d763eab2d727a42dce85f511750410
juno-choi <ililil9482@naver.com>
2021-06-03 20:52:12 +0900

    spring mvc servlet setting

---
+++ src/main/java/hello/servlet/basic/HelloServlet.java
@@ -0,0 +1,19 @@
+package hello.servlet.basic;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+// /helo로 들어왔을때 해당 servlet이 실행됨
+@WebServlet(name = "helloServlet", urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        System.out.println("HelloServlet.service");
+    }
+}
